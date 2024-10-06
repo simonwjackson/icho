32fdeed23cc04c962f434ba16750528010b12091
Simon W. Jackson <189379+simonwjackson@users.noreply.github.com>
2024-10-06 11:53:30 -0500

    refactor: update some whichkey mappings

--- plugins/whichKey.lua
+++ plugins/whichKey.lua
@@ -76,24 +76,7 @@ return {
 				separator = "➜", -- symbol used between a key and it's label
 				group = "+", -- symbol prepended to a group
 			},
-			popup_mappings = {
-				scroll_down = "<c-d>", -- binding to scroll down inside the popup
-				scroll_up = "<c-u>", -- binding to scroll up inside the popup
-			},
-			window = {
-				border = "double", -- none, single, double, shadow
-				position = "top", -- bottom, top
-				margin = { 0.1, 0.2, 0, 0.2 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-				padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-				winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
-				zindex = 1000, -- positive value to position WhichKey above other floating windows.
-			},
-			layout = {
-				height = { min = 4, max = 25 }, -- min and max height of the columns
-				width = { min = 20, max = 50 }, -- min and max width of the columns
-				spacing = 3, -- spacing between columns
-				align = "left", -- align columns left, center or right
-			},
+			preset = "modern",
 			ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
 			hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " }, -- hide mapping boilerplate
 			show_help = true, -- show a help message in the command line for using WhichKey
@@ -127,9 +110,16 @@ return {
 			},
 		},
 		init = function()
+			local wk = require("which-key")
+
 			vim.o.timeout = true
 			vim.o.timeoutlen = 100
+
+			wk.add({
+				{ "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+			})
 			-- local wk = require("which-key")
+			--   { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
 			--
 			-- local function my_lazygit()
 			-- 	cmd("tabnew")
