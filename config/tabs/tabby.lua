d6059d7c0b1be35c4f92edc0c62fe25ba5ff0369
Simon W. Jackson <github@simonwjackson.io>
2025-05-21 22:48:35 -0500

    feat: add tmux & tabby picker

--- config/tabs/tabby.lua
+++ config/tabs/tabby.lua
@@ -175,29 +175,30 @@ require("tabby").setup({
 				}
 			end),
 			-- Show regular vim tabs only if more than one exists
-			vim.fn.tabpagenr('$') > 1
-				and line.tabs().foreach(function(tab)
-				local hl = tab.is_current() and "TabLineSel" or "TabLine"
-				-- Always start with an empty tab name
-				local tab_display_name = ""
+			vim.fn.tabpagenr("$") > 1
+					and line.tabs().foreach(function(tab)
+						local hl = tab.is_current() and "TabLineSel" or "TabLine"
+						-- Always start with an empty tab name
+						local tab_display_name = ""
 
-				-- Display jump key if in jump mode, otherwise tab number
-				local tab_indicator = tab.in_jump_mode() and tab.jump_key() or tab.number()
+						-- Display jump key if in jump mode, otherwise tab number
+						local tab_indicator = tab.in_jump_mode() and tab.jump_key() or tab.number()
 
-				return {
-					line.sep(left_sep, hl, "TabLineFill"),
-					tab.is_current() and " " or " ",
-					tab_indicator,
-					tab_display_name,
-					tab.close_btn(""),
-					line.sep(right_sep, hl, "TabLineFill"),
-					hl = hl,
-					margin = " ",
-				}
-			end)
-		or {},
-		-- Show tmux windows as tabs if in tmux (always show them if in tmux)
+						return {
+							line.sep(left_sep, hl, "TabLineFill"),
+							tab.is_current() and " " or " ",
+							tab_indicator,
+							tab_display_name,
+							tab.close_btn(""),
+							line.sep(right_sep, hl, "TabLineFill"),
+							hl = hl,
+							margin = " ",
+						}
+					end)
+				or {},
+			-- Show tmux windows as tabs if in tmux (hide if only 1 window named main/master)
 			in_tmux
+					and (#tmux_windows > 1 or (#tmux_windows == 1 and tmux_windows[1].name ~= "main" and tmux_windows[1].name ~= "master"))
 					and vim.tbl_map(function(window)
 						local hl = window.active and "TabLineSel" or "TabLine"
 						local indicator = window.active and " " or "󰘬 "
@@ -213,3 +214,143 @@ require("tabby").setup({
 		}
 	end,
 })
+
+-- Function to create a picker for tmux windows and vim tabs
+local function tab_picker()
+	local items = {}
+	local in_tmux = os.getenv("TMUX_PANE") ~= nil
+
+	-- Get git worktrees
+	local worktrees = {}
+	local git_handle = io.popen("git worktree list 2>/dev/null")
+	if git_handle then
+		for line in git_handle:lines() do
+			local path, branch = line:match("([^%s]+)%s+[^%s]+%s+%[([^%]]+)%]")
+			if path and branch and not line:match("%(bare%)") then
+				local worktree_name = branch
+				worktrees[worktree_name] = {
+					name = worktree_name,
+					path = path,
+					branch = branch,
+				}
+			end
+		end
+		git_handle:close()
+	end
+
+	-- Get tmux windows and match with worktrees
+	local tmux_windows = {}
+	local matched_worktrees = {}
+	if in_tmux then
+		local handle = io.popen("tmux list-windows -F '#{window_index} #{window_name} #{window_active}'")
+		if handle then
+			for line in handle:lines() do
+				local index, name, active = line:match("(%d+) ([^%s]+) (%d+)")
+				if index then
+					tmux_windows[name] = {
+						index = tonumber(index),
+						name = name,
+						active = active == "1",
+					}
+
+					-- If this tmux window matches a worktree, mark it as matched
+					if worktrees[name] then
+						matched_worktrees[name] = true
+					end
+				end
+			end
+			handle:close()
+		end
+	end
+
+	-- Add vim tabs (skip current tab)
+	for i = 1, vim.fn.tabpagenr("$") do
+		if i ~= vim.fn.tabpagenr() then
+			local tab_info = {
+				type = "vim_tab",
+				index = i,
+				name = "Tab " .. i,
+				current = false,
+			}
+			table.insert(items, {
+				text = "  Tab " .. i,
+				value = tab_info,
+			})
+		end
+	end
+
+	-- Add tmux windows if in tmux (skip active window, skip those matched with worktrees)
+	if in_tmux then
+		for name, window in pairs(tmux_windows) do
+			if not window.active and not matched_worktrees[name] then
+				table.insert(items, {
+					text = "  " .. name .. " (tmux)",
+					value = {
+						type = "tmux_window",
+						index = window.index,
+						name = name,
+						current = false,
+					},
+				})
+			end
+		end
+	end
+
+	-- Add worktrees
+	for name, worktree in pairs(worktrees) do
+		local tmux_window = tmux_windows[name]
+		if tmux_window and not tmux_window.active then
+			-- Worktree has corresponding tmux window (show as open)
+			table.insert(items, {
+				text = " ● " .. name,
+				value = {
+					type = "worktree_open",
+					name = name,
+					tmux_index = tmux_window.index,
+					path = worktree.path,
+				},
+			})
+		elseif not tmux_window then
+			-- Worktree has no tmux window (show as available to open)
+			table.insert(items, {
+				text = "  " .. name .. " (worktree)",
+				value = {
+					type = "worktree_new",
+					name = name,
+					path = worktree.path,
+				},
+			})
+		end
+	end
+
+	-- Use vim.ui.select for the picker
+	vim.ui.select(items, {
+		prompt = "Select tab/window:",
+		format_item = function(item)
+			return item.text
+		end,
+	}, function(choice)
+		if not choice then
+			return
+		end
+
+		local info = choice.value
+		if info.type == "vim_tab" then
+			vim.cmd("tabnext " .. info.index)
+		elseif info.type == "tmux_window" then
+			vim.fn.system("tmux select-window -t " .. info.index)
+		elseif info.type == "worktree_open" then
+			-- Switch to existing tmux window
+			vim.fn.system("tmux select-window -t " .. info.tmux_index)
+		elseif info.type == "worktree_new" then
+			-- Create new tmux window and start neovim in worktree directory
+			local cmd =
+				string.format("tmux new-window -n '%s' -c '%s' 'nix run ~/Downloads/icho'", info.name, info.path)
+			vim.fn.system(cmd)
+		end
+	end)
+end
+
+-- Create command and keymap for the tab picker
+vim.api.nvim_create_user_command("TabPicker", tab_picker, {})
+vim.keymap.set("n", "<leader>tp", tab_picker, { desc = "Open tab/window picker" })
