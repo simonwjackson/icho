cc526e01b12a1a1a848501c70b773019a161af02
Simon W. Jackson <github@simonwjackson.io>
2025-11-26 21:52:34 -0700

    feat(config): add render-markdown plugin configuration

--- config/default.nix
+++ config/default.nix
@@ -34,6 +34,27 @@
   plugins = {
     trouble.enable = true;
     markdown-preview.enable = true;
+    render-markdown = {
+      enable = true;
+      settings = {
+        file_types = ["markdown"];
+        render_modes = ["n" "c" "t" "i"]; # render in insert mode too
+        debounce = 100;
+        anti_conceal = {
+          enabled = true;
+          above = 0; # buffer zone above cursor
+          below = 0; # buffer zone below cursor
+          ignore = {
+            code_background = true;
+            indent = true;
+            sign = true;
+            virtual_lines = true;
+          };
+        };
+        heading.width = "full";
+        code.width = "full";
+      };
+    };
     neo-tree = {
       enable = true;
       settings = {
