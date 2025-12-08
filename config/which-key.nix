c49048a9a858d3096aef787c5fbd4673118ae3e1
Simon W. Jackson <github@simonwjackson.io>
2025-12-08 00:58:47 -0700

    feat(which-key): add rounded border to window

--- config/which-key.nix
+++ config/which-key.nix
@@ -2,6 +2,9 @@
   plugins.which-key = {
     enable = true;
     settings = {
+      win = {
+        border = "rounded";
+      };
       spec = [
         { __unkeyed-1 = "<leader>a"; group = "AI/Opencode"; }
         { __unkeyed-1 = "<leader>g"; group = "Git"; }
