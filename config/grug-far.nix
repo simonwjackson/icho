5f06ebec0cbdc4d659944ebc74b8d20ec229853a
Simon W. Jackson <github@simonwjackson.io>
2025-12-08 01:51:13 -0700

    feat(grug-far): add window toggle and cleanup logic

--- config/grug-far.nix
+++ config/grug-far.nix
@@ -13,6 +13,14 @@
       key = "<leader>S";
       action.__raw = ''
         function()
+          -- Check if grug-far window exists and toggle it
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            if vim.bo[buf].filetype == "grug-far" then
+              vim.api.nvim_win_close(win, false)
+              return
+            end
+          end
           require('grug-far').open({ windowCreationCommand = "topleft 60vsplit" })
         end
       '';
@@ -23,6 +31,14 @@
       mode = "v";
       action.__raw = ''
         function()
+          -- Close existing grug-far window first
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            if vim.bo[buf].filetype == "grug-far" then
+              vim.api.nvim_win_close(win, false)
+              break
+            end
+          end
           require('grug-far').with_visual_selection({ windowCreationCommand = "topleft 60vsplit" })
         end
       '';
