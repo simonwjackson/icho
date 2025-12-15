fc0b70a298bbf18e0f84d2b4f69b579621af2e3a
Simon W. Jackson <github@simonwjackson.io>
2025-12-15 16:49:05 -0700

    feat(which-key): add windows keybinding group

--- config/which-key.nix
+++ config/which-key.nix
@@ -8,6 +8,11 @@
       spec = [
         { __unkeyed-1 = "<leader>a"; group = "AI/Opencode"; }
         { __unkeyed-1 = "<leader>g"; group = "Git"; }
+        {
+          __unkeyed-1 = "<leader>w";
+          group = "windows";
+          proxy = "<C-w>";
+        }
       ];
     };
   };
